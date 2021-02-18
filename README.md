2f519523f8b2b5fef23d87d62a590acccedc5b3b
Jengyoung <young_pallete@naver.com>
2021-02-18 12:03:29 +0900

    slogan-container section 구현

--- README.md
+++ README.md
@@ -2,18 +2,30 @@
 
 https://www.millie.co.kr/company/company.html?&gclid=Cj0KCQiA1KiBBhCcARIsAPWqoSq0uD57A7114k7_hU-cXbSTDO8BKxpUFUy_Dkig9dTc0a_MGb6zdHIaAi39EALw_wcB
 
+## 2021.02.18
+
+### 주요 변경 사항
+
+구현 - slogan-container 쪽 페이지 구현
+미구현 - event 미구현.
+
+#### <strong>문제 발견</strong>
+
+1. 선반 이미지들이 크기가 조정되는 현상 발생.
+   일단 주요 부분을 구현하고 수정할 예정.
+
 ## 2021.02.17
 
 ### 주요 변경 사항
 
 parcel bundler을 통한 sass 작업 실시.
-구현 - intro-Container 쪽 부분 구현.
+구현 - intro-container 쪽 부분 구현.
 미구현 - event 미구현.
 
 #### <strong>문제 발견</strong>
 
-1. 이미지 크기 및 배치 중 imgContainer에서 room-img만 크기가 조정되는 현상 발생.
-   이를 먼저 해결하고, 이벤트를 조정할 예정.
+1. <u>이미지 크기 및 배치 중 imgContainer에서 room-img만 크기가 조정되는 현상 발생.
+   이를 먼저 해결하고, 이벤트를 조정할 예정.</u> >>> 2021.02.18 해결
 
 ## 2021.02.15
 
--- dist/css.81192ade.js
+++ dist/css.81192ade.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\img\\subLogo.svg":[["subLogo.ff816756.svg","img/subLogo.svg"],"img/subLogo.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\font\\supilmyeongjo010.ttf":[["supilmyeongjo010.36fa789e.ttf","font/supilmyeongjo010.ttf"],"font/supilmyeongjo010.ttf"],"./..\\img\\subLogo.svg":[["subLogo.ff816756.svg","img/subLogo.svg"],"img/subLogo.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52264" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65510" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
