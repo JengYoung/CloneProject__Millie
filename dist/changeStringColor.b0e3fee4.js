860441dc64e4c76598ee703995c722691b70a322
Jengyoung <young_pallete@naver.com>
2021-03-16 12:43:00 +0900

    changeBoxRotationRateScrollEvent 수정

--- dist/changeStringColor.b0e3fee4.js
+++ dist/changeStringColor.b0e3fee4.js
@@ -118,13 +118,13 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/changeStringColor.js":[function(require,module,exports) {
-var colorArray = ['#9FD259', '#C6C7E6', '#74E1E5', '#75FA95', '#FFEB60', '#62EFAC'];
+var colorArray = ['#9FD259', '#60FAC4', '#C6C7E6', '#74E1E5', '#75FA95', '#FFEB60', '#62EFAC'];
 var strongString = document.querySelector('#genre-introduction-container strong');
 window.addEventListener('scroll', function () {
   var strongStringY = strongString.getBoundingClientRect().y;
 
-  if (strongStringY < 700 && strongStringY > 400) {
-    strongString.style.color = colorArray[parseInt(strongString.getBoundingClientRect().y % 6)];
+  if (strongStringY < innerHeight && strongStringY > innerHeight * 0.3) {
+    strongString.style.color = colorArray[parseInt(strongString.getBoundingClientRect().y % 7)];
   } else {
     strongString.style.color = '#FFEB60';
   }
--- js/changeStringColor.js
+++ js/changeStringColor.js
@@ -1,10 +1,10 @@
 
-const colorArray = ['#9FD259','#C6C7E6','#74E1E5','#75FA95','#FFEB60','#62EFAC']
+const colorArray = ['#9FD259','#60FAC4','#C6C7E6','#74E1E5','#75FA95','#FFEB60','#62EFAC']
 const strongString = document.querySelector('#genre-introduction-container strong')
 window.addEventListener('scroll', () => {
     const strongStringY = strongString.getBoundingClientRect().y
-    if (strongStringY < 700 && strongStringY > 400) {
-        strongString.style.color = colorArray[parseInt(strongString.getBoundingClientRect().y % 6)]
+    if (strongStringY < innerHeight && strongStringY > innerHeight * 0.3) {
+        strongString.style.color = colorArray[parseInt(strongString.getBoundingClientRect().y % 7)]
     } else {
         strongString.style.color = '#FFEB60';
     }
